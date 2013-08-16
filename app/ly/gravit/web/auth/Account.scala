d2b08a29ce96ce0dd4de719a7324c3730b6c744d
Giancarlo Inductivo <gian@dynamicobjx.com>
2013-08-16 18:18:57 +0800

    few more tweaks.. need to setup user authentication with parse next..

--- app/ly/gravit/web/auth/Account.scala
+++ app/ly/gravit/web/auth/Account.scala
@@ -1,5 +1,7 @@
 package ly.gravit.web.auth
 
+import scala.collection.mutable
+
 /**
  * Created with IntelliJ IDEA.
  * User: ginduc
@@ -7,12 +9,24 @@ package ly.gravit.web.auth
  * Time: 1:14 PM
  * To change this template use File | Settings | File Templates.
  */
-case class Account(id: Int, email: String, password: String, name: String, permission: Permission)
+case class Account(id: String, email: String, password: String, name: String, permission: Permission)
 
 object Account {
-  val default = Account(1, "ned@flanders.com", "password", "Ned Flanders", Administrator)
+  val default = Account("1", "ned@flanders.com", "password", "Ned Flanders", Administrator);
 
   def findById(id: String): Option[Account] = Option(default)
+  def findByEmail(email: String): Option[Account] = Option(default)
 
-  def authenticate(email: String, password: String): Option[Account] = Option(default)
+  def authenticate(email: String, password: String): Option[Account] = {
+    findByEmail(email) match {
+      case Some(user) => {
+         if (user.email.equals(email) && user.password.equals(password)) {
+           Option(user)
+         } else {
+           None
+         }
+      }
+      case None => None
+    }
+  }
 }
