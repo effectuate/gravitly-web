a6ff3d73d10d7bcd5cd1ad84072c9602664c4c09
unknown <cllayba>
2013-08-22 11:12:22 +0800

    adin upload

--- app/controllers/Admin.scala
+++ app/controllers/Admin.scala
@@ -4,6 +4,8 @@ import play.api.mvc._
 import views.html._
 import jp.t2v.lab.play2.auth.AuthElement
 import ly.gravit.web.auth._
+import play.api.data._
+import play.api.data.Forms._
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,11 +16,31 @@ import ly.gravit.web.auth._
  */
 object Admin extends Controller with AuthElement with AuthConfigImpl {
 
-  def index = StackAction(AuthorityKey -> Administrator) { implicit request =>
-    Ok(admin.index(loggedIn))
+  //def index = Action(AuthorityKey -> Administrator) { implicit request =>
+  def index = Action { implicit request =>
+    //Ok(admin.index(loggedIn))
+    Ok(admin.index(null))
+
   }
 
-  def upload = StackAction(AuthorityKey -> Administrator) { implicit request =>
+  //def upload = StackAction(AuthorityKey -> Administrator) { implicit request =>
+  def upload = Action { implicit request =>
     Ok(admin.upload())
+  }
+
+  val uploadForm = Form(
+      "adminUpload" -> text
+  )
+  def submitUpload = Action { implicit request =>
+    uploadForm.bindFromRequest.fold(
+      formWithErrors => {
+       BadRequest
+        Ok("not ok")
+      },
+      value => {
+        Redirect("list") //TODO change the redirect page
+      }
+    )
+
   }
 }
