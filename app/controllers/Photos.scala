41551c43743b35cb96e6e81b40e05693b7789d0c
Giancarlo Inductivo <gian@dynamicobjx.com>
2013-08-22 10:57:23 +0800

    added parse module to libs dir..

--- app/controllers/Photos.scala
+++ app/controllers/Photos.scala
@@ -2,7 +2,7 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-//import dynobjx.play.parseapi.ParseApi
+import dynobjx.play.parseapi.ParseApi
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,7 +13,7 @@ import play.api.mvc._
  */
 object Photos extends Controller {
   def index(id: String) = Action {
-    //ParseApi.get("User", "Ij8j7HtrxC")
+    ParseApi.get("User", "Ij8j7HtrxC")
     Ok(views.html.photo("Hello World"))
   }
 }
